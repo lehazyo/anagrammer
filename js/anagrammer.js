b07c09e6047fce7e65f2d5d35cfb735f7e3d1e74
Алексей Сергиенко <lehazyo@gmail.com>
2020-04-06 09:20:36 +0300

    sorting after letters change fix

--- js/anagrammer.js
+++ js/anagrammer.js
@@ -14,9 +14,13 @@ class Anagrammer {
     this.letters_wrapper.innerHTML = "";
 
     for(var i=0;i<letters_array.length;i++) {
+      var letter = letters_array[i];
+      if(!letter.match(/[a-zа-яё]/ig)) {
+        continue;
+      }
       var letter_el = document.createElement("div");
       letter_el.className = "letter";
-      letter_el.textContent = letters_array[i];
+      letter_el.textContent = letter;
       this.letters_wrapper.appendChild(letter_el);
     }
 
